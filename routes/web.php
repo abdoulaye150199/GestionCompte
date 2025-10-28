70607ca45563d624bb7037342ebce0eb7f18cedc
abdoulaye150199 <abdoulaye150199@gmail.com>
2025-10-28 23:29:38 +0000

    docs: serve Swagger UI under /abdoulaye.diallo/docs (+assets)

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,23 @@ Route::get('/docs', function () {
     abort(404);
 });
 
+// Also serve the docs under the user's prefixed path: /abdoulaye.diallo/docs
+Route::get('/abdoulaye.diallo/docs', function () {
+    $path = public_path('docs/index.html');
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    abort(404);
+});
+
+// Serve static assets for the prefixed docs path
+Route::get('/abdoulaye.diallo/docs/asset/{asset}', function ($asset) {
+    $path = public_path('docs/asset/' . $asset);
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    abort(404);
+})->where('asset', '.*');
+
 // Temporary protected diagnostic endpoint (requires DIAG_SECRET or APP_DEBUG=true)
 Route::get('/abdoulaye.diallo/_diagnose', [\App\Http\Controllers\DiagController::class, 'index']);
