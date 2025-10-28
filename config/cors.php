154945902e3f5c166b84bfbaf4613a50c3c5d6a1
abdoulaye150199 <abdoulaye150199@gmail.com>
2025-10-28 21:41:39 +0000

    ok

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://127.0.0.1:8000', 'http://localhost:8000'],
+    'allowed_origins' => ['*', 'http://127.0.0.1:8000', 'http://localhost:8000', 'https://gestioncompte-api.onrender.com', 'https://compte-api-hjvy.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
