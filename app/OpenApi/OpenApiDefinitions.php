0353e5cd04f71335d61df5a089999e56d9a52b3e
abdoulaye150199 <abdoulaye150199@gmail.com>
2025-10-28 21:54:20 +0000

    ok

--- app/OpenApi/OpenApiDefinitions.php
+++ app/OpenApi/OpenApiDefinitions.php
@@ -3,31 +3,16 @@
 namespace App\OpenApi;
 
 /**
- * @OA\Info(
- *     version="1.0.0",
- *     title="API de Gestion de Comptes",
- *     description="API pour la gestion des comptes bancaires",
- *     @OA\Contact(
- *         email="admin@example.com"
- *     )
- * )
- * 
  * @OA\Server(
  *     url="http://localhost:8000",
  *     description="Serveur local"
  * )
- * 
+
  * @OA\Server(
  *     url="https://abdoulaye.diallo.api",
  *     description="Serveur de production"
  * )
- * 
- * @OA\SecurityScheme(
- *     securityScheme="bearerAuth",
- *     type="http",
- *     scheme="bearer",
- *     bearerFormat="JWT"
- * )
+
  */
 class OpenApiDefinitions
 {
