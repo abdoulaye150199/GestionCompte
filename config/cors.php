30e80afb6a1883d7d98507c603d5d85690e7a352
abdoulaye150199 <abdoulaye150199@gmail.com>
2025-11-01 17:54:14 +0000

    ok

--- config/cors.php
+++ config/cors.php
@@ -4,17 +4,17 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'docs/*'],
 
-    // Allow all methods
     'allowed_methods' => ['*'],
 
-    // Explicit allowed origins: local frontend and production Render domain.
-    // If you ever need credentials (cookies/Authorization with credentials),
-    // add the exact origin here and set 'supports_credentials' => true.
-    'allowed_origins' => ['http://localhost:8000', 'https://compte-api-transaction-abdoulaye-diallo.onrender.com', 'https://gestioncompte-2.onrender.com'],
+    'allowed_origins' => [
+        'http://127.0.0.1:8000',
+        'http://localhost:8000',
+        'https://compte-api-transaction-abdoulaye-diallo.onrender.com',
+        'https://gestioncompte-2.onrender.com'
+    ],
 
     'allowed_origins_patterns' => [],
 
-    // Allow all headers
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [
@@ -22,10 +22,7 @@ return [
         'X-Auth-Token'
     ],
 
-    // Cache preflight responses for 1 day
     'max_age' => 86400,
 
-    // Do not support credentials for now (if you enable wildcard '*' you must set this to false)
     'supports_credentials' => true,
-
 ];
